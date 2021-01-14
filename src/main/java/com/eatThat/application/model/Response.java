1b9fcfc033503ab85c9b73c564f526f144060af6
muhammad.danish3 <muhammad.danish3@jazz.com.pk>
2021-01-14 20:44:03 +0500

    config updates

--- src/main/java/com/eatThat/application/model/Response.java
+++ src/main/java/com/eatThat/application/model/Response.java
@@ -2,7 +2,6 @@ package com.eatThat.application.model;
 
 import java.util.ArrayList;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Response<T> {
@@ -23,6 +22,7 @@ public class Response<T> {
 		this.message = message;
 	}
 	
+	@SuppressWarnings("unchecked")
 	public void setData(User usr) {
 		this.ResponseData = (T) usr;
 	}
@@ -34,14 +34,17 @@ public class Response<T> {
 		this.ResponseData = null;
 	}
 	
+	@SuppressWarnings("unchecked")
 	public void setData(ArrayList<FoodItem> footItem) {
 		this.ResponseData = (T) footItem;
 	}
 	
+	@SuppressWarnings("unchecked")
 	public void setDietPlans(ArrayList<DietPlans> dietPlans) {
 		this.ResponseData = (T) dietPlans;
 	}
 	
+	@SuppressWarnings("unchecked")
 	public void setCategories(ArrayList<FoodCategories> foodCategories) {
 		this.ResponseData = (T) foodCategories;
 	}
